e788209f3fb082c5b5bed2161b10e5a207b4ba1f
Cheng Wei <michael1996623@gmail.com>
2017-06-30 04:22:55 +0000

    Part1 done

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,11 +7,68 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
+  attr_accessor :word, :guesses, :wrong_guesses
+
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @list_of_try = Array.new
+    @list_of_correct_char = Array.new 
+    @num_of_fail = 0
   end
-
+  
+  #main guess method 
+  def guess(g)
+    if g.to_s.empty? or g.to_s < 'A' or g.to_s > 'z'
+      raise ArgumentError.new("Invalid guess")
+    end 
+    
+    # Chang all characters to lower case 
+    g = g.downcase 
+    
+    if @list_of_try.include? g
+      return false
+    else
+      @list_of_try.push g
+      
+      if @word.include? g 
+        @guesses = g
+        @list_of_correct_char.push g
+      else
+        @wrong_guesses = g 
+        @num_of_fail += 1
+      end  
+      return true
+      
+    end
+  end
+  
+  #Current guessing status
+  def word_with_guesses
+    @ans = ''
+    @word.split("").each do |i|
+      if @list_of_correct_char.include? i 
+        @ans << i
+      else
+        @ans << '-'
+      end
+    end
+    return @ans 
+  end
+    
+  #Game status 
+  def check_win_or_lose
+    if @num_of_fail >= 7 
+      return :lose 
+    elsif not word_with_guesses.to_s.include? "-"
+      return :win 
+    else 
+      return :play
+    end 
+  end
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
